36e7fddf89748a36447c66b009208945c3a7d0f5
davidxv15 <davidxvaz@gmail.com>
2025-01-23 23:38:32 -0700

    depens

--- code-galaxy/src/main.tsx
+++ code-galaxy/src/main.tsx
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const App = () => {
-  return <div className="text-center text-xl">Hello, Template!</div>;
+  return (
+    <div className="text-center text-xl">
+      Hello, Template!
+    </div>
+  );
 };
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(

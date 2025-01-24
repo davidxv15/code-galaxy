3b4de91582456fd833763b5c3db4b37bf9ceb04d
davidxv15 <davidxvaz@gmail.com>
2025-01-23 22:50:11 -0700

    index.css

--- code-galaxy/src/main.tsx
+++ code-galaxy/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const App = () => {
+  return <div className="text-center text-xl">Hello, Template!</div>;
+};
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);

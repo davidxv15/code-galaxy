fc4b157e5a16a5db52bb46a440c893fa0ace32b6
davidxv15 <davidxvaz@gmail.com>
2025-01-24 00:01:21 -0700

    fix

--- code-galaxy/src/App.tsx
+++ code-galaxy/src/App.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function App() {
-  return (
-    <>
-      <div>Hello, Template!</div>
-    </>
-  );
+  return <div>Hello, Code Galaxy!</div>;
 }
 
 export default App;

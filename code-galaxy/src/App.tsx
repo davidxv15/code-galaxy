ad168d68a155402b17b37c7628da8e2a391851bb
davidxv15 <davidxvaz@gmail.com>
2025-03-19 22:20:24 -0700

    refator(App): boilerplate setup

--- code-galaxy/src/App.tsx
+++ code-galaxy/src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const App: React.FC = () => {
-  return ( 
-    <div>
-      <Component ></Component>
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">🚀 Code Galaxy!</h1>
     </div>
   );
-};
+}
 
 export default App;
